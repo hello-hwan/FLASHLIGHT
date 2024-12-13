f1e6481875fcf8cc887c3a6ea808b912e38fafb8
leejh1129 <leejh09876@naver.com>
2024-12-13 16:56:23 +0900

    2024-12-13

--- client/src/views/standardInfo/standardInfoApp.vue
+++ client/src/views/standardInfo/standardInfoApp.vue
@@ -1,3 +1,5 @@
 <template>
     <h1>기준정보</h1>
 </template>
+
+
--- server/database/sqls/standard_info.js
+++ server/database/sqls/standard_info.js
@@ -6,6 +6,38 @@ const cmmn =
        ,cmmn_name
 FROM cmmn`;
 
+const bom =
+`SELECT bom_code
+       ,prdlst_code
+       ,prdist_name
+       ,prdctn_qy
+       ,sumry
+FROM bom`;
+
+
+// BOM 상세보기 쿼리
+const bomInfo =
+`SELECT b.prdlst_code
+       ,b.prdist_name
+       ,b.prdctn_qy
+       ,bc.cmpds_no
+       ,bc.cmpds_prdlst_name
+       ,bc.stndrd_y
+       ,bc.unit
+       ,bc.cnsum_count
+FROM bom b JOIN bom_cmpds bc
+on b.bom_code = bc.bom_code
+WHERE b.bom_code = ?`;
+
+// BOM 등록 쿼리
+const bomInsert = 
+`INSERT INTO bom_cmpds
+SET ? `;
+
+
 module.exports = {
-  cmmn
+  cmmn,
+  bom,
+  bomInfo,
+  bomInsert
 };
