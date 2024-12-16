ba130eb32e7c8592088ff88ec13abb21be93cead
gihwan <rlghksrla12@naver.com>
2024-12-16 16:54:55 +0900

    241216_조회페이지 70%완성

--- client/src/router/standardInfoRoute.js
+++ client/src/router/standardInfoRoute.js
@@ -1,6 +1,6 @@
 //기준정보 라우트
 import processFlowInput from '@/views/standardInfo/processFlowInput.vue';
-import processFlowView from '@/views/standardInfo/processFlowView.vue';
+//import processFlowView from '@/views/standardInfo/processFlowView.vue';
 
 const standardInfoRoutes = [
     {
@@ -11,12 +11,12 @@ const standardInfoRoutes = [
         path: 'processFlowInput',
         name: 'processFlowInput',
         component: processFlowInput
-    }, 
+    }/*, 
     {
         path: 'processFlowView',
         name: 'processFlowView',
         component: processFlowView
-    }
+    }*/
 ];
 
 export default standardInfoRoutes;
