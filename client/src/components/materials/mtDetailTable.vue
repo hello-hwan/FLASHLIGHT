34c835444bbe9bef5abeac38fa21a5460deb56ba
Taebaek Chio <112chamchi@naver.com>
2024-12-19 17:52:42 +0900

    241219_출고관리 출고등록 작업중

--- client/src/components/materials/mtDetailTable.vue
+++ client/src/components/materials/mtDetailTable.vue
@@ -1,14 +1,23 @@
 <template>
-    <div style="display: inline-block;
-                width: 62%">
+  <div id="#req_details">
+    <div style="margin-bottom: 10px;
+                text-align: right;
+                height: 38px;">
+      <button type="button" class="btn btn-primary" 
+       v-bind:disabled="isdlivyBtn"
+       @click="registData"
+       style="line-height: 1px; color: #fff;">
+        자재 전체 출고</button>
+    </div>
+
+    <div>
         <!--요청 자재 상세목록-->
         <v-card
           class="mx-auto"
           style=" width: 100%;
                   border-bottom-right-radius: 13px;
                   border-bottom-left-radius: 13px;
-                  display: inline-block;"
-        >
+                  display: inline-block;">
           <template v-slot:title>
             <span class="font-weight-black">요청 자재 상세목록</span>
           </template>
@@ -18,12 +27,14 @@
                   :defaultColDef="defaultColDef"
                   :rowData="rowData"
                   :gridOptions="gridOptions"
-                  class="ag-theme-alpine"
-                  @grid-ready = "onGridReady">
+                  class="ag-theme-alpine">
               </AgGridVue>
           </v-card-text>
         </v-card>
     </div>
+
+
+  </div>
 </template>
 
 <script setup>
@@ -33,6 +44,13 @@ ModuleRegistry.registerModules([AllCommunityModule]);
 import { ref, watch, defineProps } from "vue";
 import axios from "axios";
 import { ajaxUrl } from '@/utils/commons.js';
+import { useToast } from 'primevue/usetoast';
+
+const toast = useToast();
+
+//출고버튼 보여주는걸 결정하는 boolean타입변수
+let isdlivyBtn = ref(true);
+console.log(isdlivyBtn);
 
 //부모로부터 가져옴
 const props = defineProps(['code']);
@@ -49,18 +67,41 @@ const ColDefs = [
   { field: "req_qy", headerName:"요청수량"},
   { field: "lot", headerName:"lot", flex: 2.5},
   { field: "lot_qy", headerName: "출고수량"},
-  { field: "unit", headerName:"단위", flex: 0.8}
+  { field: "unit", headerName:"단위", flex: 0.8},
+  { field: 'prdcnt_code', hide: true, suppressToolPanel: true},
+  { field: 'empl_no', headerName:"출고 담당자", hide: true, suppressToolPanel: true}
 ];
-
 
 //요청명 리스트
 const getReqDetails = async (reqCode) => {
     //상세 자재 수량 가져오기
     let result = await axios.get(`${ajaxUrl}/mtril/mtRequestDetails/${reqCode}`)
                             .catch(err => console.log(err));
-    //행 정보를 변수에 담음
-    rowData.value = result.data;
-    console.log(result);
+    
+    console.log('result', result.data.length);
+    console.log(result.data[(result.data.length-1)].state);
+
+    if(result.data[(result.data.length-1)].state == 'fail') {
+      //자재가 부족한 경우 처리
+      //rowData.value = [{req_name : ''}]; //임의의 데이터를 한행 추가 보이진 않음. 이 작업이 없을경우 재고가 있는 행을 선택한 후 없는 행을 선택했을때 행이 비워지지 않음
+      result.data.pop();  //배열 마지막 요소 제거
+      rowData.value = result.data;
+
+      //자재가 충분하지 않은경우 안내메시지
+      toast.add({ severity: 'warn', summary: '자재부족', detail: '자재가 부족합니다\n재고를 확인해주세요.', life: 3000 });
+
+      isdlivyBtn.value = true;
+      console.log('비활성화: ', isdlivyBtn.value);
+
+    } else {
+      result.data.pop();  //배열 마지막 요소 제거
+
+      //자재가 충분한 경우, 행 정보를 변수에 담음
+      rowData.value = result.data;
+
+      isdlivyBtn.value = false;
+      console.log('활성화: ', isdlivyBtn.value);
+    }
     //console.log('getReqDetails: 작동확인');
 };
 
@@ -74,6 +115,7 @@ watch(() => props.code, (newValue) => {
     getReqDetails(newValue);
  
     //console.log('watch: 값이 변경됨');
+
 });
 
 //ag grid 옵션 설정
@@ -83,8 +125,19 @@ const gridOptions = {
       defaultColDef: {
           flex: 1,
           minWidth: 10
-      }
-
+      },
+      
 };
 
+//출고테이블 등록, 재고 수량 업데이트
+const registData = async() => {
+  console.log(rowData.value[0]);
+  let result = await axios.post(`${ajaxUrl}/mtril/mtDlivy`,rowData.value );
+  console.log(result);
+//   if(result.state == 'success') {
+//     toast.add({ severity: 'success', summary: '출고 성공', detail: '처리가 완료되었습니다.', life: 3000 });
+//   } else {
+//     toast.add({ severity: 'error', summary: '출고 실패', detail: '문제가 생겼습니다.\n관리자에게 문의해주세요.', life: 3000 });
+//   }
+}
 </script>
--- server/service/materials_service.js
+++ server/service/materials_service.js
@@ -42,9 +42,12 @@ const requestList = async() => {
 const requestMt = async(reqCode) => {
     //요청 리스트 가져오기
     let reqMtList = await mariaDB.query('mt_requestDetails', reqCode);
-    
+
     //새로운 데이터를 담을 배열 선언(객체들의 배열이 됨)
     let dataList = [];
+
+    //참일 경우 로트에 재고가 충분한 경우.
+    let tableGetReady = true;
 
     //자재코드를 가져오기 위한 for문
     for(let i=0; i<reqMtList.length; i++) {
@@ -54,9 +57,25 @@ const requestMt = async(reqCode) => {
         //요청수량
         let reqQy = reqMtList[i].qy;
 
+        //로트 수량 총합
+        let sumLotQy = 0;
+
         for(let j=0; j<lotListEachMt.length; j++) {
             //로트 수량
             let lotQy = lotListEachMt[j].mtril_qy;
+            //로트별 수량 확인
+            //console.log(reqMtList[i].mt_name, lotListEachMt[j].mtril_lot, lotListEachMt[j].mtril_qy);
+
+            //요청 수량
+            //console.log('요청수량', reqMtList[i].qy);
+
+            //기본 로트 수량
+            //console.log('번경전', lotListEachMt[j].mtril_qy);
+
+            //절대값 변경
+            //console.log('변경값', (lotQy - Math.abs(reqQy - lotQy)));
+            //로트 수량 총 합 구하기
+            sumLotQy += lotListEachMt[j].mtril_qy;
 
             //데이터를 담을 새로운 객체 선언
             let newObj = {
@@ -67,7 +86,8 @@ const requestMt = async(reqCode) => {
                 req_qy : reqMtList[i].qy,           //요청수량
                 lot : lotListEachMt[j].mtril_lot,   //로트
                 lot_qy : (reqQy - lotQy) < 0 ? (lotQy - Math.abs(reqQy - lotQy)) : lotQy, //로트 수량
-                unit : reqMtList[i].unit            //단위
+                unit : reqMtList[i].unit,            //단위
+                prdctn_code : reqMtList[i].prdctn_code
             };
             //요청 수량에서 재고 수량 빼기
             reqQy -= lotQy;
@@ -76,34 +96,57 @@ const requestMt = async(reqCode) => {
             dataList.push(newObj);
 
             //자재 수량에서 로트수량을 뺐을때 0보다 작으면 해당 자재에는 출고수량을 채웠다는 의미
-            if(reqQy < 0) {
+            if(reqQy <= 0) {
                 break;
             };
         }
-    }
-    //요청수량과 재고수량을 비교해서 값이 같이 않으면 state: 'fail'전송
-    /*
-    for(let i=0; i<dataList.length; i++) {
-        console.log(dataList[i]);
-        if((Number(dataList[i].req_qy) != Number(dataList[i].lot_qy))) {
-            dataList = [{state : 'fail'}];
-            break;
-        };
-    };
-    */
 
-    return dataList;
+        //요청 수량이 재고 수량의 총 합보다 많을 경우 tableGetReady를 false로 만들어 return에서 임의의 값을 리턴하게 만듦
+        if (reqMtList[i].qy > sumLotQy) {
+            tableGetReady = false;
+        };
+    }
+    //확인  
+    //console.log(dataList);
+
+    if(tableGetReady) {
+        //재고가 충분한 경우
+        dataList.push({state : 'success'});
+        return dataList;
+    } else {
+        //어느 하나라도 자재의 재고가 부족한 경우
+        dataList.push({state : 'fail'});
+        return dataList;
+    };
 }
 
-//출고 등록 후 처리 여부 업데이트
+//출고 등록 + 처리 여부 업데이트 + 로트 수량 빼기
 const dlivyMt = async(dlivyInfo) => {
-    let result = await mariaDB.query('mt_requestCheckOut', [...dlivyInfo]);
-    
-    if( result.insertId != null){
-        return 'success'; 
-    }else{
-    return 'fail';
+    console.log(dlivyInfo);
+    let resultSum = 0;
+
+    for(let i=0; i<dlivyInfo.length; i++) {
+        let dataArr = [ dlivyInfo[i].lot,
+                        dlivyInfo[i].req_code,
+                        dlivyInfo[i].req_name,
+                        dlivyInfo[i].mt_name,
+                        dlivyInfo[i].mt_code,
+                        dlivyInfo[i].req_qy,
+                        dlivyInfo[i].lot_qy,
+                        dlivyInfo[i].empl_no,
+                        dlivyInfo[i].prdcnt_code
+                    ];
+        let result = await mariaDB.query('mt_requestCheckOut', dataArr);    
+        //성공하면 1을 리턴, 실패하면 0을 리턴
+        resultSum += result;
     };
+
+    //성공한 수의 총합과 등록하는 데이터의 개수를 비교, 같으면 성공, 다르면 실패.
+    if(resultSum = dlivyInfo.length) {
+        return 'success';
+    } else {
+        return 'fail';
+    }
 }
 module.exports = {
     returnMt,
