670d1a0be1314068c713ebf75cb17380998bfbe8
hello-hwan <rlghksrla12@naver.com>
2024-12-23 14:43:01 +0900

    Revert "241223-막힌부분"

--- client/src/views/production/productionMain.vue
+++ client/src/views/production/productionMain.vue
@@ -3,11 +3,11 @@
     <div>
       <p>생산 일정 안내 </p>
       <input type="date" v-model="day">
-      <input type="text" placeholder="제품코드" v-model="prd" @keydown="getprdlist()">
-      <div v-if="prdlist.length > 0">
-        <p v-for="prdst in prdlist" :key="prdst.prdlst_code" @click="getprdcode(prdst.prdlst_code)">{{ prdst.prdlst_code + ', ' + prdst.prdlst_name }}</p>
+      <input type="text" placeholder="제품코드" v-model="prd">
+      <div v-show="prd != null">
+        <p v-for="prdst in prdlist" :key="prdst.prdlist_code">{{ prdst.prdlst_code + ', ' + prdst.prdlst_name }}</p>
       </div>
-      <button type="button" @click="getanotherlist(prd, day)">검색</button>
+      <button type="button">검색</button>
     </div>
     <div>
       <table class="table">
@@ -44,7 +44,7 @@
         <tbody>
           <tr v-for="eqp in eqplist" :key="eqp.eqp_code">
             <th>{{ eqp.model_nm }}</th>
-            <td v-for="drct in drctlist" :key="drct.prdctn_code" :colspan="drct.drct_time" v-show="eqp.model_nm = drct.model_nm"> {{ drct.prd_nm + ' 제품\n'+ drct.procs_nm + ' 작업' }}</td>
+            <td v-for="drct in drctlist" :key="drct.prdctn_code" :colspan="drct.drct_time" > {{ drct.prd_nm + ' 제품\n'+ drct.procs_nm + ' 작업' }}</td>
           </tr>
 
         </tbody>
@@ -62,6 +62,7 @@
 
   // 제품 검색 할 코드
   const prd = ref('');
+  prd.value = '스';
   
 
   // 날짜 표기할 변수
@@ -71,7 +72,7 @@
   
   // 설비리스트 담을 변수
   const eqplist = ref([]);
-  
+
   // 설비 불러올 함수
   const getEqp = async () => {
     let result = await axios.get(`${ajaxUrl}/prod/eqplist`)
@@ -81,43 +82,29 @@
   // 설비 불러올 함수 실행
   getEqp();
 
-
   // 생산일정 담을 변수
   const drctlist = ref([]);
+
   // 생산일정 불러올 함수
   const getdrct = async () => {
    // let result = await axios.get(`${ajaxUrl}/prod/drctlist`)
    let result = await axios.get(`${ajaxUrl}/prod/seldrct`)
                            .catch(err => console.log(err));
     drctlist.value = result.data;
-
   };
   // 생산일정 불러올 함수 실행
   getdrct();
 
-  
-
-  // 제품 키워드로 검색 내용 담을 변수
+  // 제품 검색
   const prdlist = ref([]);
-  // 제품 키워드로 검색할 함수
-  const getprdlist = async () => {
-    let result = await axios.get(`${ajaxUrl}/prod/prdlist/${prd.value}`)
+
+  const getprd = async (prd) => {
+    let result = await axios.get(`${ajaxUrl}/prod/prdlist/${prd}`)
                             .catch(err => console.log(err));
     prdlist.value = result.data;
   };
-  // 검색 클릭시 실행할 함수
-  const getprdcode = (code) => {
-    prd.value = code;
-    prdlist.value = [];
-  };
-
-  // 검색 클릭시 실행할 함수
-  const getanotherlist = async (prd_code, day_str) => {
-
-    let result = await axios.get(`${ajaxUrl}/prod/srcdrct`, )
-                            .catch(err => console.log(err));
-    drctlist.value = result.data;
-  };
+  getprd(prd);
+  
 
  
 </script>
